fc04d8189a66525b91d9031e6624038b6042d732
Angelo Giacco <giacco.angelo.s@gmail.com>
2020-05-15 18:24:13 +0100

    make gh-page build only

---
+++ build/precache-manifest.ae5330992ac3211c6569d5d3aa8f74bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3adc680c6b5408a536fb96ab3b3438",
+    "url": "/Marko/index.html"
+  },
+  {
+    "revision": "c29bed67eddffbc09e2a",
+    "url": "/Marko/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "c94c9c4026b59ffa704b",
+    "url": "/Marko/static/css/main.1eb7e059.chunk.css"
+  },
+  {
+    "revision": "c29bed67eddffbc09e2a",
+    "url": "/Marko/static/js/2.61c0f5a8.chunk.js"
+  },
+  {
+    "revision": "bc5c820b7e29b66c7c049f027bcdc037",
+    "url": "/Marko/static/js/2.61c0f5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94c9c4026b59ffa704b",
+    "url": "/Marko/static/js/main.8650df10.chunk.js"
+  },
+  {
+    "revision": "ebadf10e05bfa23e1811",
+    "url": "/Marko/static/js/runtime-main.76659cc7.js"
+  },
+  {
+    "revision": "3f9c37f377cac346af7cea8adc4f0803",
+    "url": "/Marko/static/media/HKGrotesk-Italic.3f9c37f3.otf"
+  },
+  {
+    "revision": "cab8839a909b408392b7b3147c2afd23",
+    "url": "/Marko/static/media/HKGrotesk-Regular.cab8839a.otf"
+  },
+  {
+    "revision": "507739170000c10b7ddf7bfa59a4952d",
+    "url": "/Marko/static/media/HKGrotesk-SemiBold.50773917.otf"
+  },
+  {
+    "revision": "3ecc2801d223b01878d055123fbe3f38",
+    "url": "/Marko/static/media/HKGrotesk-SemiBoldItalic.3ecc2801.otf"
+  }
+]);
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as App } from "./App/App";
-export { default as Dashboard } from "./Dashboard/Dashboard";
-export { default as Editor } from "./Editor/Editor";
-export { default as SignIn } from "./SignIn/SignIn";
